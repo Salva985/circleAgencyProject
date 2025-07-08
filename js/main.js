22ce2a678779fc3c4d2aa16fe2ce9fb1ca064618
salvatore marchese <smarchese985@yahoo.co.uk>
2025-07-08 11:58:50 +0200

    footer and scroll arrow finished

--- js/main.js
+++ js/main.js
@@ -32,3 +32,19 @@ window.addEventListener('load', () => {
         console.error('Failed to load projects:', err);
       });
   });
+
+
+
+  const scrollBtn = document.getElementById("scrollToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.add("show");
+    } else {
+      scrollBtn.classList.remove("show");
+    }
+  });
+  
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }); 
